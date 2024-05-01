447b927eccd5cc2c0207dc3eba19c32c442e7b96
manogna145 <manogna.krk@gmail.com>
2024-05-01 17:07:29 +0530

    Updated server.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running on the port number ${PORT}`));
 
 //Configuration (MONGODB)
-var curl = "mongodb://localhost:27017";
+var curl = "mongodb+srv://admin:admin@cluster0.yrmqoh1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 var client = new MongoClient(curl); 
 
 //TESTING
